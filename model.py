38d10772b28c0b309c1c7c860000a6ef6cfc2565
whybe-choi <choi980912@khu.ac.kr>
2024-07-10 10:56:32 +0900

    feat: Define RMSNorm block

--- model.py
+++ model.py
@@ -55,6 +55,23 @@ def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device
     x_out = x_out.reshape(x.shape)
     return x_out.type_as(x).to(device)
 
+class RMSNorm(nn.Module):
+
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        # The gamma parameter
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x: torch.Tensor):
+        # (batch, seq_len, dim) * (batch, seq_len, 1) -> (batch, seq_len, dim)
+        # rsqrt: 1 / sqrt(x)
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+    
+    def forward(self, x: torch.Tensor):
+        # (dim) * (batch, seq_len, dim) -> (batch, seq_len, dim)
+        return self.weight * self._norm(x.float()).type_as(x)
+
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
         super().__init__()
