086726dc4eaed0aaa197a5c351e077b5e4441974
whybe-choi <choi980912@khu.ac.kr>
2024-07-10 11:15:24 +0900

    feat: Define TransformerBlock

--- model.py
+++ model.py
@@ -72,6 +72,28 @@ class RMSNorm(nn.Module):
         # (dim) * (batch, seq_len, dim) -> (batch, seq_len, dim)
         return self.weight * self._norm(x.float()).type_as(x)
 
+class TransformerBlock(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        # Normalize before the self attention
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        # Normalize before the feed forward block
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        # (batch, seq_len, dim) + (batch, seq_len, dim) -> (batch, seq_len, dim)
+        h = x + self.attention(self.attention_norm(x), start_pos, freqs_complex)
+        out = h + self.feed_forward(self.ffn_norm(h))
+        return out
+
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
         super().__init__()
