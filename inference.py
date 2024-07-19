a3ad8798f28deab6aeb1a99cdd276c763a44d6d0
whybe-choi <choi980912@khu.ac.kr>
2024-07-19 22:57:17 +0900

    feat: Create inference.py

---
+++ inference.py
@@ -0,0 +1,16 @@
+from typing import Optional
+import torch
+import time
+from pathlib import Path
+import json
+from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+
+from model import ModelArgs, Transformer
+
+class LLaMA:
+    
+    def __init__(self, model: Transformer, tokenizer: SentencePieceProcessor, model_args: ModelArgs):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = model_args
