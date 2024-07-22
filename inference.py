f1dc732ce145a3ef1d535e707e680fd3f54d0f64
whybe-choi <choi980912@khu.ac.kr>
2024-07-22 23:47:06 +0900

    feat: Define build method

--- inference.py
+++ inference.py
@@ -4,7 +4,7 @@ import time
 from pathlib import Path
 import json
 from sentencepiece import SentencePieceProcessor
-from tqdm import tqdm
+from  tqdm import tqdm
 
 from model import ModelArgs, Transformer
 
@@ -14,3 +14,31 @@ class LLaMA:
         self.model = model
         self.tokenizer = tokenizer
         self.args = model_args
+
+    @staticmethod
+    def build(checkpoints_dir: str, tokenizer_path: str, load_model: bool, max_seq_len: int, max_batch_size: int, device: str):
+        prev_time = time.time()
+        if load_model:
+            checkpoints = sorted(Path(checkpoints_dir).glob("*.pth"))
+            assert len(checkpoints) > 0, "No checkpoints files found"
+            chk_path = checkpoints[0]
+            print(f"Loading checkpoint {chk_path}")
+            checkpoint = torch.load(chk_path, map_location="cpu")
+            print(f"Loaded checkpoint in {time.time() - prev_time:.2f}s")
+            prev_tim = time.time()
+
+        with open(Path(checkpoints_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len, 
+            max_batch_size=max_batch_size,
+            device=device,
+            **params
+        )
+
+        tokenizer = SentencePieceProcessor()
+        tokenizer.load(tokenizer_path)
+        model_args.vocab_Size = tokenizer.vocab_size()
+
+        if device == "cuda":
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
